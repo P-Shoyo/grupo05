57f383fb31c93658fb996cc757d4d3d821d309b4
LuanCollyns <luan.csilva@bandtec.com.br>
2020-09-23 21:43:11 -0300

    removi os selects

--- Banco de Dados/ProjetoPI3Final- Grupo5.sql
+++ Banco de Dados/ProjetoPI3Final- Grupo5.sql
@@ -12,7 +12,6 @@ ADM char (1),
 Telefone varchar (20)
 );
  
- select * from Usuario; 
  
  insert into Usuario values (1, 'Bruno Teixeira', 'Bruno@gmail.com', '123.456.789-26', '64.786.435-3', 'plioson123', '1', '(11)4002-8922'),
 							(2, 'Isabella Pires', 'Isabella@gmail.com', '123.456.789-27', '64.786.435-4', 'plioson123', '1', '(11)4002-8923'),
@@ -35,8 +34,6 @@ insert into ControleT values (1, '12/12/12', '30,5'),
 							 (2, '20/09/20', '27,0');
  
  
- select * from ControleT; 
- 
  create table ControleU (
  id int primary key,
  Momento varchar (20),
@@ -46,7 +43,6 @@ insert into ControleT values (1, '12/12/12', '30,5'),
 insert into ControleU values (1, '10/10/10', '20,5%'),
 							 (2, '21/09/20', '40,3%'); 
 
-select * from ControleU;
 
 create table RelatorioU (
 id int primary key,
@@ -60,8 +56,6 @@ DiferencaU varchar (6)
 insert into RelatorioU values (1, '10/10/12', '20,0%', '50,5%', '35.2%', '30,5%'),
 							  (2, '22/09/20', '22,0%', '53,7%', '37,5%', '31,7%');
 
-select * from RelatorioU;
-
 create table RelatorioT (
 id int primary key,
 DataHoje varchar (50),
@@ -73,5 +67,4 @@ DiferencaT varchar (6)
 
 insert into RelatorioT values (1, '10/10/12', '25,4', '46,8', '36,1', '21,4'),
 							  (2, '22/09/20', '22,0', '32,7', '27,3', '10,7');
-
-select * from RelatorioT;
+   
